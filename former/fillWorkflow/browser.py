21d031c823f52c5f1daa0960be4ba4b61b71f697
Lukasz <prosowiec@gmail.com>
2026-04-14 22:53:35 +0200

    Fix airlfow broswer

--- former/fillWorkflow/browser.py
+++ former/fillWorkflow/browser.py
@@ -35,6 +35,7 @@ def get_realistic_context(p):
             '--disable-setuid-sandbox',
             '--disable-web-security',
             '--disable-features=IsolateOrigins,site-per-process',
+            "--disable-dev-shm-usage",
         ],
         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         viewport={'width': 1920, 'height': 1080},
